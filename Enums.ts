0564dfd91ee76091c55c7208047dc7e8a8870939
duahcim <s2125sh53@outlook.com>
2026-03-09 09:31:19 +0800

    Add Enums for PlayerId, ZoneType, UnitType, and Keyword

---
+++ Enums.ts
@@ -0,0 +1,10 @@
+export enum PlayerId { Player1, Player2 }
+export enum ZoneType { FriendlySupport, Frontline, EnemySupport }
+export enum UnitType { Infantry, Tank, Artillery }
+export enum Keyword {
+  Flash = '闪击',
+  Guard = '守护',
+  Ambush = '伏击',
+  Smoke = '烟幕',
+  // 可扩展
+}
