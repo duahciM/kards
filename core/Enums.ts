97012fde2bd3c1aad525b00857b92cb3420b295d
duahcim <s2125sh53@outlook.com>
2026-03-16 12:28:14 +0800

    Delete core directory

--- core/Enums.ts
+++
@@ -1,9 +0,0 @@
-export enum PlayerId { Player1, Player2 }
-export enum ZoneType { FriendlySupport, Frontline, EnemySupport }
-export enum UnitType { Infantry, Tank, Artillery }
-export enum Keyword {
-  Flash = '闪击',
-  Guard = '守护',
-  Ambush = '伏击',
-  Smoke = '烟幕',
-}
